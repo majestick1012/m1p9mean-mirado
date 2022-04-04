2a4d6fdd5427c3bc3bf85e1d144801003dcaeac8
Mirado RANDRIAMBOLOLONA <miradorandriambololona@gmail.com>
2022-04-04 15:56:05 +0300

    Intégration JWT lors du Login

--- server.js
+++ server.js
@@ -1,125 +1,67 @@
 const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const db = require('./config/db');
+
 var express = require('express');
 var nodemailer = require('nodemailer');
 const path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 
 // Variables d'environnement
-var connectionString = process.env.CONNECTION_STRING;
-var mode = process.env.NODE_ENV;
+var mode = process.env.NODE_ENV || "Development";
 var port = process.env.PORT || 3000;
+
+// Routes
+const clientRoutes = require("./routes/client");
+const restaurantRoutes = require("./routes/restaurant");
 
 // ExpressJS
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
-
-var db = {};
+app.use("/api/client", clientRoutes);
+app.use("/api/restaurant", restaurantRoutes);
 
 app.get('/', function (request, response) {
-    console.log('Bienvenue');
-    response.sendFile(path.join(__dirname + '/public/index.html'));
+  console.log('Bienvenue');
+  response.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.get('/gmail', function (request, response) {
-    // OAuth
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD,
-            clientId: process.env.OAUTH_CLIENTID,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.OAUTH_REFRESH_TOKEN
-        }
-    });
-    let mailOptions = {
-        from: process.env.MAIL_USERNAME,
-        to: 'majestick1012@gmail.com',
-        subject: '[TEST] Nodemailer Project MEAN',
-        text: 'Hi from your nodemailer project.'
-    };
-    transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Email test sent successfully");
-        }
-        response.redirect('/');
-    });
-});
-
-
-MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(client => {
-    console.log('Connected to ' + mode + ' database ');
-    db = client.db('sample_restaurants');
-    var restaurantsCollection = db.collection('restaurants');
-});
-
-app.get('/restaurants', (req, res) => {
-    db.collection('restaurants').find().toArray().then(results => {
-        console.log(results);
-        res.redirect('/');
-    }).catch(error => console.error(error));
-
-});
-
-app.post('/restaurants', (req, res) => {
-    db.collection('restaurants').insertOne(req.body).then(result => {
-        res.redirect('/');
-        console.log(result);
-    }).catch(error => { console.log(error) });
-    // console.log('insertion fait');
-    // console.log(req.body);
-});
-
-app.put('/restaurants', (req, res) => {
-
-    db.collection('restaurants').findOneAndUpdate(
-        { name: req.body.name },
-        {
-            $set:
-            {
-                name: req.body.namenode,
-                quote: req.body.quote
-            }
-        },
-        {
-            upsert: true
-        }
-
-    ).then(result => { res.redirect('/'); console.log(req.body.name); })
-        .catch(error => console.error(error))
-});
-
-app.delete('/restaurants', (req, res) => {
-    console.log(req.body.name);
-    db.collection('restaurants').deleteOne(
-        { name: req.body.name }
-
-    )
-        .then(result => {
-            if (result.deletedCount === 0) {
-                //return res.json('No quote to delete');
-
-                console.log("No restaurant to delete");
-            }
-            else {
-                console.log("deleted quote");
-            }
-            res.redirect('/');
-        })
-        .catch(error => console.error(error))
+app.get('/api/gmail', function (request, response) {
+  // OAuth
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
+  });
+  let mailOptions = {
+    from: process.env.MAIL_USERNAME,
+    to: 'majestick1012@gmail.com',
+    subject: '[TEST] Nodemailer Project MEAN',
+    text: 'Hi from your nodemailer project.'
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email test sent successfully");
+    }
+    response.redirect('/');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', function loadserver() {
-    console.log('Mode: ' + mode);
-    console.log(`Launching the app ${process.env.APP_NAME}`);
-    console.log('Listening on port: ' + PORT);
-    console.log('Connecting to ' + mode + ' database...');
+  console.log('Mode: ' + mode);
+  console.log(`Launching the app ${process.env.APP_NAME}`);
+  console.log('Listening on port: ' + PORT);
+  console.log('Connecting to ' + mode + ' database...');
 });
